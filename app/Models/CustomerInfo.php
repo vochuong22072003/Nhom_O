1e7b274c7612256408d331c0f7060af9db0575e9
dodesu <do204contact@gmail.com>
2024-11-21 10:14:38 +0700

    fix: add new customer info when registered

--- app/Models/CustomerInfo.php
+++ app/Models/CustomerInfo.php
@@ -9,13 +9,19 @@ class CustomerInfo extends Model
 {
     use HasFactory;
 
-    protected $table = 'customers_info';
+    protected $table = 'customer_info';
 
     // Khai báo khóa chính
     protected $primaryKey = 'cus_id';
+    public $incrementing = false;
+    public $timestamps = false;
+    protected $casts = [
+        'birth_date' => 'datetime',
+    ];
 
+    protected $guard = 'cus_id';
     protected $fillable = [
-        'cus_id',   // Khóa ngoại từ bảng customers
+        'cus_id', 
         'cus_name',
         'birth_date',
         'cus_phone',
@@ -23,6 +29,38 @@ class CustomerInfo extends Model
         'gender',
         'cus_avt',
     ];
+    public static function getEnumGenders()
+    {
+        $type = \DB::selectOne("SHOW COLUMNS FROM customer_info WHERE Field = ?", ['gender'])->Type;
+
+        preg_match("/^enum\((.*)\)$/", $type, $matches);
+
+        if (isset($matches[1])) {
+            // Tách các giá trị ENUM và loại bỏ dấu nháy đơn
+            return array_map(function ($value) {
+                return trim($value, "'");
+            }, explode(',', $matches[1]));
+        }
+
+        return [];
+    }
+    /**
+     * Return url of avatar
+     * Just put it in src:
+     * @return string
+     */
+    public function getAvtUrl(): string
+    {
+        if (!empty($this->cus_avt)) {
+            return \Storage::disk('upload')->url($this->cus_avt);
+        }
+        return asset('client/images/upload/avt-default.png');
+    }
+    public function getAtrl()
+    {
+
+        return "";
+    }
 
     // relationship
     public function customer()
