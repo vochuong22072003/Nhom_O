169ff87702dbe25a397800ce179d271f5ee4a97a
dodesu <do204contact@gmail.com>
2024-11-22 21:21:23 +0700

    feat: verify email in setting

--- resources/views/client/account-setting-partials/general.blade.php
+++ resources/views/client/account-setting-partials/general.blade.php
@@ -4,6 +4,10 @@
     <!-- general beggin -->
     <div class="tab-pane fade active show" id="account-general">
         <div class="card-body">
+            <form id="send-verification" method="POST" action="{{ route('verification.send') }}">
+                @csrf
+            </form>
+
             <form id="general-form" action="{{ route('setting.general-update') }}" method="POST" enctype="multipart/form-data">
                 @csrf
                 @method('patch')
@@ -23,21 +27,39 @@
                     <label class="form-label">E-mail</label>
                     <input type="email" class="form-control mb-1" name="email" value="{{ old('email', $cus->email) }}">
 
-                    {{--  Email verify  --}}
-                    @if (!$cus->hasVerifiedEmail())
-                        <div class="alert alert-warning mt-3">
-                            Your email is not confirmed. Please check your inbox.<br>
-                            <a href="javascript:void(0)">Resend confirmation</a>
-                        </div>
-                    @endif
-
                     @error('email')
                         <div class="alert alert-danger mt-3">
                             {{ $errors->first('email') }}
                         </div>
                     @enderror
+
+                    {{--  Email verify  --}}
+                    @if ($cus instanceof \Illuminate\Contracts\Auth\MustVerifyEmail && !$cus->hasVerifiedEmail())
+                        <div class="alert alert-warning mt-3">
+                            <span class="mb-3">
+                                Your email is not confirmed. Please confirm now.<br>
+                            </span>
+                        </div>
+
+                        @if (session('status') == 'verification-link-sent')
+                            <div class="mb-4 text-success">
+                                A new verification link has been sent to the email address you provided during registration.
+                            </div>
+                        @endif
+
+                        <button type="submit" form="send-verification" class="btn-sendemail">
+                            Send Verification Email
+                        </button>
+                    @else
+                    <div class="alert alert-success mt-3">
+                        <span class="mb-3">
+                            Your email is confirmed.<br>
+                        </span>
+                    </div>
+                    @endif
                 </div>
             </form>
+
         </div>
 
     </div>
